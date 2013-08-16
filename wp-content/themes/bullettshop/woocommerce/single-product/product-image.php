1a11c51a21d0b9523a36d3af4a33e2e4c339522e
rememberlenny <lkbgift@gmail.com>
2013-08-16 18:09:52 -0400

    Replaced images class

--- wp-content/themes/bullettshop/woocommerce/single-product/product-image.php
+++ wp-content/themes/bullettshop/woocommerce/single-product/product-image.php
@@ -12,31 +12,33 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 global $post, $woocommerce, $product;
 
 ?>
-<div class="images small-12 large-8 column">
+<div class=" small-12 large-8 column">
+	<div class="images">
 
-	<?php
-		if ( has_post_thumbnail() ) {
+		<?php
+			if ( has_post_thumbnail() ) {
 
-			$image       		= get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ) );
-			$image_title 		= esc_attr( get_the_title( get_post_thumbnail_id() ) );
-			$image_link  		= wp_get_attachment_url( get_post_thumbnail_id() );
-			$attachment_count   = count( $product->get_gallery_attachment_ids() );
+				$image       		= get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ) );
+				$image_title 		= esc_attr( get_the_title( get_post_thumbnail_id() ) );
+				$image_link  		= wp_get_attachment_url( get_post_thumbnail_id() );
+				$attachment_count   = count( $product->get_gallery_attachment_ids() );
 
-			if ( $attachment_count > 0 ) {
-				$gallery = '[product-gallery]';
+				if ( $attachment_count > 0 ) {
+					$gallery = '[product-gallery]';
+				} else {
+					$gallery = '';
+				}
+
+				echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<a href="%s" itemprop="image" class="large-10 column woocommerce-main-image zoom" title="%s"  rel="prettyPhoto' . $gallery . '">%s</a>', $image_link, $image_title, $image ), $post->ID );
+
 			} else {
-				$gallery = '';
+
+				echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="Placeholder" />', woocommerce_placeholder_img_src() ), $post->ID );
+
 			}
+		?>
 
-			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<a href="%s" itemprop="image" class="large-10 column woocommerce-main-image zoom" title="%s"  rel="prettyPhoto' . $gallery . '">%s</a>', $image_link, $image_title, $image ), $post->ID );
+		<?php do_action( 'woocommerce_product_thumbnails' ); ?>
 
-		} else {
-
-			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="Placeholder" />', woocommerce_placeholder_img_src() ), $post->ID );
-
-		}
-	?>
-
-	<?php do_action( 'woocommerce_product_thumbnails' ); ?>
-
+	</div>
 </div>
