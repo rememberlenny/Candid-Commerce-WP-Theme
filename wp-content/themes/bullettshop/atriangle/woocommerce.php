77423f7f4ee84a51f28bab1ccbc9514af05c3cea
rememberlenny <lkbgift@gmail.com>
2013-08-16 21:20:54 -0400

    Force check on rollover fix

--- wp-content/themes/bullettshop/atriangle/woocommerce.php
+++ wp-content/themes/bullettshop/atriangle/woocommerce.php
@@ -59,9 +59,9 @@ function atc_pagination(){
 add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_rollover_image', 12 );
 
 function woocommerce_rollover_image(){
-    if(get_field('product_rollover_image')):
+    if(get_field('product_rollover_image')){
         echo '<img src="' . get_field('product_rollover_image') . '" class="rollover" style="display:none;" height="330">';
-    endif;
+    }
 }
 
 ?>
