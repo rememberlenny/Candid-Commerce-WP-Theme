a34d2fded85f5e9abe046c2e793e0eeccde4634d
rememberlenny <lkbgift@gmail.com>
2013-08-19 15:04:07 -0400

    Arrow updates

--- wp-content/themes/bullettshop/footer.php
+++ wp-content/themes/bullettshop/footer.php
@@ -13,16 +13,8 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer row" role="contentinfo">
-		<div class="site-info column">
-			<!-- Site was developed and implemented by Leonard Kiyoshi Bogdonoff (http://rememberlenny.com) from A Triangle Corporation (http://atriangle.com) -->
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'bullettshop' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'bullettshop' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'bullettshop' ), 'bullettshop', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
 		<div class="foot">
-			<div class="logo">
-				
-			</div>
+			<div class="logo"></div>
 		</div>
 	
 	</footer><!-- #colophon -->
