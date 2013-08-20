818721949eb4ded003d05b1eed6532d66b3c2e13
rememberlenny <lkbgift@gmail.com>
2013-08-20 11:59:04 -0400

    Checkout

--- wp-content/themes/bullettshop/woocommerce/cart/cart.php
+++ wp-content/themes/bullettshop/woocommerce/cart/cart.php
@@ -20,7 +20,7 @@ $woocommerce->show_messages();
 
 <?php do_action( 'woocommerce_before_cart_table' ); ?>
 
-<table class="shop_table cart" cellspacing="0">
+<table class="shop_table cart" cellspacing="0" float:right>
 	<thead>
 		<tr>
 			<th class="product-remove">&nbsp;</th>
