3b65acbc05f74dc2dbb6e9e473c086d5fe96ceb8
rememberlenny <lkbgift@gmail.com>
2013-08-21 11:42:23 -0400

    Add modal for ipad links

--- wp-content/themes/bullettshop/atriangle/atc-modals.php
+++ wp-content/themes/bullettshop/atriangle/atc-modals.php
@@ -2,19 +2,21 @@
 
 function ipadPopUpScript(){
 ?>
-  jQuery("a[href$='http://bullettstoreat.wpengine.com/p/ipad-magazine-subscription/']").attr('data-reveal-id', 'ipadPopUp');
+  jQuery("a[href$='http://bullettstoreat.wpengine.com/p/ipad-magazine-subscr'+
+  'iption/']").attr('data-reveal-id', 'ipadPopUp');
 <?php
 }
 
 function ipadPopUpModal(){
 ?>
-  jQuery('#colophon').after('<div id="ipadPopUp" class="reveal-modal"><h2>We'+ 
-  'are opening a new tab.</h2><p class="lead">The following link will open a'+
+  jQuery('#colophon').after('<div id="ipadPopUp" class="reveal-modal"><h2>Th'+ 
+  'is will open a new tab.</h2><p class="lead">The following link will open a'+
   ' new window to the iTunes AppStore. You can subscribe to the Bullett maga'+
   'zine from there.</p><p>If you have any items in your shopping cart, they '+
   'won\'t go anywhere (as long as you don\'t close the window).</p><a href="'+
-  'https://itunes.apple.com/us/app/bullett/id557294227?mt=8" class="button" '+
-  'target="_blank"><\/a><a class="close-reveal-modal">&#215;<\/a><\/div>');
+  'https://itunes.apple.com/us/app/bullett/id557294227?mt=8" class="button l'+
+  'arge single_add_to_cart_button" target="_blank">Get it!<\/a><a class="clo'+
+  'se-reveal-modal">&#215;<\/a><\/div>');
 <?php
 }
 
