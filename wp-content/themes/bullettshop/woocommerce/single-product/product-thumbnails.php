abf484b2dfb9d68fa8a4cc2fae2dcff15d14c82f
rememberlenny <lkbgift@gmail.com>
2013-08-20 20:24:21 -0400

    block grid

--- wp-content/themes/bullettshop/woocommerce/single-product/product-thumbnails.php
+++ wp-content/themes/bullettshop/woocommerce/single-product/product-thumbnails.php
@@ -16,40 +16,35 @@ $attachment_ids = $product->get_gallery_attachment_ids();
 if ( $attachment_ids ) {
 	?>
 	<div class="thumbnails large-2 row column">
-		<ul class="small-block-grid-3 large-block-grid-1">
-			
-			<?php
+	<?php
 
-				$loop = 0;
-				$columns = apply_filters( 'woocommerce_product_thumbnails_columns', 1 );
+		$loop = 0;
+		$columns = apply_filters( 'woocommerce_product_thumbnails_columns', 1 );
 
-				foreach ( $attachment_ids as $attachment_id ) {
+		foreach ( $attachment_ids as $attachment_id ) {
 
-					$classes = array( 'zoom' );
+			$classes = array( 'zoom' );
 
-					if ( $loop == 0 || $loop % $columns == 0 )
-						$classes[] = 'first';
+			if ( $loop == 0 || $loop % $columns == 0 )
+				$classes[] = 'first';
 
-					if ( ( $loop + 1 ) % $columns == 0 )
-						$classes[] = 'last';
+			if ( ( $loop + 1 ) % $columns == 0 )
+				$classes[] = 'last';
 
-					$image_link = wp_get_attachment_url( $attachment_id );
+			$image_link = wp_get_attachment_url( $attachment_id );
 
-					if ( ! $image_link )
-						continue;
+			if ( ! $image_link )
+				continue;
 
-					$image       = wp_get_attachment_image( $attachment_id, apply_filters( 'single_product_small_thumbnail_size', 'shop_thumbnail' ) );
-					$image_class = esc_attr( implode( ' ', $classes ) );
-					$image_title = esc_attr( get_the_title( $attachment_id ) );
+			$image       = wp_get_attachment_image( $attachment_id, apply_filters( 'single_product_small_thumbnail_size', 'shop_thumbnail' ) );
+			$image_class = esc_attr( implode( ' ', $classes ) );
+			$image_title = esc_attr( get_the_title( $attachment_id ) );
 
-					echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '<li><a href="%s" class="%s" title="%s"  rel="prettyPhoto[product-gallery]">%s</a></li>', $image_link, $image_class, $image_title, $image ), $attachment_id, $post->ID, $image_class );
+			echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '<a href="%s" class="%s" title="%s"  rel="prettyPhoto[product-gallery]">%s</a>', $image_link, $image_class, $image_title, $image ), $attachment_id, $post->ID, $image_class );
 
-					$loop++;
-				}
+			$loop++;
+		}
 
-			?>
-
-		</ul>
-	</div>
+	?></div>
 	<?php
 }
