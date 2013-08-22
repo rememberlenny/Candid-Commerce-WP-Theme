7892774f305e2922f5765ecab6944f4a9f69f0de
rememberlenny <lkbgift@gmail.com>
2013-08-22 09:41:13 -0400

    Google analytics

--- wp-content/themes/bullettshop/header.php
+++ wp-content/themes/bullettshop/header.php
@@ -19,7 +19,16 @@
 </head>
 
 <body <?php body_class(); ?>>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-43414732-1', 'bullettmedia.com');
+  ga('send', 'pageview');
+
+</script>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
 
