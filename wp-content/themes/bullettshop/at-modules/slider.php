8a256afbb227a38b7d4245aab8c7e5513c5a9cec
rememberlenny <lkbgift@gmail.com>
2013-08-22 12:06:47 -0400

    Options custom

--- wp-content/themes/bullettshop/at-modules/slider.php
+++ wp-content/themes/bullettshop/at-modules/slider.php
@@ -3,10 +3,10 @@
   <div id="slide-case" class="slideshow-wrapper row slide-case">
     <div class="preloader"></div>
     <ul data-orbit data-options="animation:fade; bullets: false; slide_number: false; timer: false;">
-      <?php while(has_sub_field('image_slider_repeater')): ?>
+      <?php while(has_sub_field('image_slider_repeater', 'option')): ?>
         <li>
-          <a href="<?php the_sub_field('image_slide_link_destination'); ?>">
-            <img src="<?php the_sub_field('image_slide_photo_content'); ?>" alt="<?php bloginfo( 'name' ); ?>">
+          <a href="<?php the_sub_field('image_slide_link_destination', 'option'); ?>">
+            <img src="<?php the_sub_field('image_slide_photo_content', 'option'); ?>" alt="<?php bloginfo( 'name' ); ?>">
           </a>
         </li>
       <?php endwhile; ?>
