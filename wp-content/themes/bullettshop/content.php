a95173f27dc5404ef1727469ae2dc357974d1b4c
rememberlenny <lkbgift@gmail.com>
2013-08-28 14:44:37 -0500

    decomment

--- wp-content/themes/bullettshop/content.php
+++ wp-content/themes/bullettshop/content.php
@@ -54,10 +54,6 @@
 			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
 
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'bullettshop' ), __( '1 Comment', 'bullettshop' ), __( '% Comments', 'bullettshop' ) ); ?></span>
-		<?php endif; ?>
-
 		<?php edit_post_link( __( 'Edit', 'bullettshop' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
--- wp-content/themes/bullettshop/image.php
+++ wp-content/themes/bullettshop/image.php
@@ -64,27 +64,10 @@ get_header();
 				</div><!-- .entry-content -->
 
 				<footer class="entry-meta">
-					<?php
-						if ( comments_open() && pings_open() ) : // Comments and trackbacks open
-							printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'bullettshop' ), esc_url( get_trackback_url() ) );
-						elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open
-							printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'bullettshop' ), esc_url( get_trackback_url() ) );
-						elseif ( comments_open() && ! pings_open() ) : // Only comments open
-							 _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'bullettshop' );
-						elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed
-							_e( 'Both comments and trackbacks are currently closed.', 'bullettshop' );
-						endif;
 
-						edit_post_link( __( 'Edit', 'bullettshop' ), ' <span class="edit-link">', '</span>' );
-					?>
 				</footer><!-- .entry-meta -->
 			</article><!-- #post-## -->
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() )
-					comments_template();
-			?>
 
 		<?php endwhile; // end of the loop. ?>
 
