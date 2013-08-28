be5f2a1c64692562b91b90b7127132db669bda73
rememberlenny <lkbgift@gmail.com>
2013-08-28 16:18:41 -0500

    Remove double box

--- wp-content/themes/bullettshop/woocommerce/content-product-related.php
+++ wp-content/themes/bullettshop/woocommerce/content-product-related.php
@@ -47,7 +47,6 @@ $classes = array();
 			 */
 			do_action( 'woocommerce_before_shop_loop_item_title' );
 		?>
-		<div class="text-box">
 			
 			
 
@@ -62,7 +61,6 @@ $classes = array();
 				do_action( 'woocommerce_after_shop_loop_item_title' );
 			?>
 
-		</div>
 	</a>
 
 	<?php do_action( 'woocommerce_after_shop_loop_item' ); ?>
