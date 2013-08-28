c45adef29a386b13fc4e41f900a742d0faa5def4
rememberlenny <lkbgift@gmail.com>
2013-08-28 17:02:35 -0500

    the ID

--- wp-content/themes/bullettshop/woocommerce/content-product.php
+++ wp-content/themes/bullettshop/woocommerce/content-product.php
@@ -32,9 +32,6 @@ $woocommerce_loop['loop']++;
 $classes = array();
 
 ?>
-<?php 
-		echo '<!--'. the_ID() .'-->';
-?>  
 
 <li <?php post_class( $classes ); ?>>
 	<?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
