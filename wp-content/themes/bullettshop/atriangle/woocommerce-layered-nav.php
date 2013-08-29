c5d8c8a2545cf15fa1f576441f5b538c2d9b6d20
rememberlenny <lkbgift@gmail.com>
2013-08-29 15:56:47 -0400

    Layed nav

--- wp-content/themes/bullettshop/atriangle/woocommerce-layered-nav.php
+++ wp-content/themes/bullettshop/atriangle/woocommerce-layered-nav.php
@@ -2,16 +2,16 @@
 
   // add_filter(‘sod_ajax_layered_nav_containers’, ‘aln_add_custom_container’);
 
-  function aln_add_custom_container($containers){
-      $containers[] = ‘#content';
-      $containers[] = ‘#content';
-      return $containers;
-  }
+  // function aln_add_custom_container($containers){
+  //     $containers[] = ‘#content';
+  //     $containers[] = ‘#content';
+  //     return $containers;
+  // }
 
-  // add_filter(‘sod_ajax_layered_nav_product_container’, ‘aln_product_container’);
-  function aln_product_container($product_container){
-    //Enter either the class or id of the container that holds your products
-    return ‘.products’;
-  }
+  // // add_filter(‘sod_ajax_layered_nav_product_container’, ‘aln_product_container’);
+  // function aln_product_container($product_container){
+  //   //Enter either the class or id of the container that holds your products
+  //   return ‘.products’;
+  // }
 
 ?>
