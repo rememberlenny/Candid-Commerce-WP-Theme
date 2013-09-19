3fc6da66be2eb6cb23c839e550c462fd805593e0
rememberlenny <leonard_bogdonoff@condenast.com>
2013-09-19 11:00:00 +0100

    Restructure theme at base of repo

---
+++ README.md
@@ -0,0 +1,15 @@
+![Sample Image](https://raw.githubusercontent.com/rememberlenny/Candid-Commerce-WP-Theme/master/home-example.png)
+
+# Candid Commerce WordPress Theme
+
+This is a store theme based on the WooCommerce plugin. 
+
+# Screenshots
+
+![Sample Image](https://raw.githubusercontent.com/rememberlenny/Candid-Commerce-WP-Theme/master/single-example.png)
+
+![Sample Image](https://raw.githubusercontent.com/rememberlenny/Candid-Commerce-WP-Theme/master/index-example.png)
+
+# Note
+
+If you use this theme, please contact Leonard K. Bogdonoff (lenny@atriangle.com). Thank you.
--- index.php
+++ index.php
@@ -1,17 +1,46 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file.
  *
- * @package WordPress
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package bullettshop
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+get_header(); ?>
 
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php bullettshop_content_nav( 'nav-below' ); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'no-results', 'index' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
